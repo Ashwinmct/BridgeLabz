21735f3065f202cd761e3ec73f4ca93d07f5207b
Ashwinmct <63235668+Ashwinmct@users.noreply.github.com>
2020-04-17 09:03:15 +0530

    Add files via upload

---
+++ forloop/powerof2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+#To find and print the values 2 and it's powers in table
+read -p "Enter the n value " n;
+printf "n 2^n"
+for ((i=1;i<=n;i++))
+do
+	product=1;
+	for ((j=0;j<n;j++))
+	do
+		product=$((product*2));
+	done
+	echo "$i $product";
+done
