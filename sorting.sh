3022833a4af1d922e955172c63d692f6a62357c0
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-04-18 12:45:17 +0530

    Refractor. added usecase4 to sorting.sh

--- sorting.sh
+++ sorting.sh
@@ -3,11 +3,16 @@
 read -p "Enter first number " a;
 read -p "Enter Second number " b;
 read -p "Enter Third Number "  c;
-#usecase2
+#usecase 2
 #To compute a+b*c
 result1=$((a+b*c));
 #usecase 3
 #To complete a*b+c
 result2=$((a*b+c));
+#Usecase 4
 #To complete  c+a/b
 result3=`awk "BEGIN{print $((c+(a/b)))}"`;
+#Use case 5 a%b+c
+#To compute a%b+c
+result4=$((a%(b+c)));
+
