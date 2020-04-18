8ac18191c421061633bb091c22171b64949a9a8e
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-04-18 12:14:22 +0530

    Refractor. added usecase2-sorting.sh to repository

--- usecase2-sorting.sh
+++ usecase2-sorting.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 read -p "Enter first number " a;
 read -p "Enter Second number" b;
-read -p "Enter Third Number"  c;
+read -p "Enter Third Number "  c;
 #To compute a+b*c
 result=$((a+b*c));
