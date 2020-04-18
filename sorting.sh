7a2e9e16d69a4cb9b7df0faa9d1a802aeaeabb95
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-04-18 12:41:53 +0530

    Refractor. added usecase3 to sorting.sh

--- sorting.sh
+++ sorting.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -x
 #usecase1
 read -p "Enter first number " a;
-read -p "Enter Second number" b;
+read -p "Enter Second number " b;
 read -p "Enter Third Number "  c;
 #usecase2
 #To compute a+b*c
 result1=$((a+b*c));
 #usecase 3
+#To complete a*b+c
 result2=$((a*b+c));
-
+#To complete  c+a/b
+result3=`awk "BEGIN{print $((c+(a/b)))}"`;
