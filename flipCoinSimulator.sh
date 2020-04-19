319e808ae0367cfa4d77fd36ffe07dc75f79e22b
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-04-19 18:38:23 +0530

    Refractor. added Usecase 4 to flipCoinSimulator.sh

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -73,4 +73,61 @@ echo "Percentage of TT= `awk "BEGIN{print $((tt*100/n))}"`";
 echo "Percentage of HT= `awk "BEGIN{print $((ht*100/n))}"`";
 echo "Percentage of TH= `awk "BEGIN{print $((th*100/n))}"`";
 
+#Usecase 4
+#flip coin for n times using loop
+#store Triplets in a dictionary
+#find percentage of Singlets
+declare -A triplets
+hhh=0
+hht=0
+hth=0
+htt=0
+thh=0
+tht=0
+tth=0
+ttt=0
+for (( i=0;i<n;i++ ))
+do
+	res="$( flipCoin )""$( flipCoin )""$( flipCoin )";
+	triplets[$i]=$res;
+	case $res in
+		"HHH")
+			hhh=$((hhh+1));
+			;;
+		"HHT")
+			hht=$((hht+1));
+			;;
+		"HTH")
+			hth=$((hth+1));
+			;;
+		"HTT")
+			htt=$((htt+1));
+			;;
+		"THH")
+			thh=$((thh+1));
+			;;
+		"THT")
+			tht=$((tht+1));
+			;;
+		"TTH")
+			tth=$((tth+1));
+			;;
+		"TTT")
+			ttt=$((ttt+1));
+			;;
+	esac
+done
+echo "Generated triplets:"
+echo ${triplets[@]};
+echo "HHH=$hhh HHT=$hht HTH=$hth HTT=$htt THH=$thh THT=$tht TTH=$tth TTT=$ttt"
+echo "Percentage of HHH= `awk "BEGIN{print $((hhh*100/n))}"`";
+echo "Percentage of TTH= `awk "BEGIN{print $((tth*100/n))}"`";
+echo "Percentage of HTH= `awk "BEGIN{print $((hth*100/n))}"`";
+echo "Percentage of THH= `awk "BEGIN{print $((thh*100/n))}"`";
+echo "Percentage of HHT= `awk "BEGIN{print $((hht*100/n))}"`";
+echo "Percentage of TTT= `awk "BEGIN{print $((ttt*100/n))}"`";
+echo "Percentage of HTT= `awk "BEGIN{print $((htt*100/n))}"`";
+echo "Percentage of THT= `awk "BEGIN{print $((tht*100/n))}"`";
+
+
 
