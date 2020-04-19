b915e2f73f903537f54cbb25d610871b3dbab4f3
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-04-19 18:18:24 +0530

    Refractor. added Usecase 3 to flipCoinSimulator.sh

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -37,3 +37,40 @@ echo "Percentage of Tails= `awk "BEGIN{print $((tails*100/n))}"`";
 
 
 
+#Usecase 2
+#flip coin for n times using loop
+#store singlets in a dictionary
+#find percentage of Singlets
+declare -A doublets
+hh=0
+ht=0
+th=0
+tt=0
+for (( i=0;i<n;i++ ))
+do
+	res="$( flipCoin )""$( flipCoin )"
+	doublets[$i]=$res;
+	case $res in
+		"HH")
+			hh=$((hh+1));
+			;;
+		"HT")
+			ht=$((ht+1));
+			;;
+		"TH")
+			th=$((th+1));
+			;;
+		"TT")
+			tt=$((tt+1));
+			;;
+	esac
+done
+echo "Generated doublets:"
+echo ${doublets[@]};
+echo "HH=$hh HT=$ht TH=$th TT=$tt"
+echo "Percentage of HH= `awk "BEGIN{print $((hh*100/n))}"`";
+echo "Percentage of TT= `awk "BEGIN{print $((tt*100/n))}"`";
+echo "Percentage of HT= `awk "BEGIN{print $((ht*100/n))}"`";
+echo "Percentage of TH= `awk "BEGIN{print $((th*100/n))}"`";
+
+
