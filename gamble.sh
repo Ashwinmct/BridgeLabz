06624dc9a2eec4de1b93aff173031551ae695d77
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-04-20 10:45:43 +0530

    Refractor. added usecase 2 to gamble.sh

--- gamble.sh
+++ gamble.sh
@@ -6,9 +6,15 @@
 stake=100;
 goal=200;
 isWin=1;
-money=$stake;
-while [ $money -lt $goal ] && [ $money -gt 0 ]
+
+#Usecase 2
+#A gambler bets $1 so  either win or loose $1
+days=100
+for ((i=0;i<days;i++))
 do
+	money=$stake;
+	while [ $money -lt $goal ] && [ $money -gt 0 ]
+	do
 		gamble=$((RANDOM%2));
 		if [ $gamble -eq $isWin ]
 		then
@@ -16,13 +22,7 @@ do
 		else
 			money=$((money-1));
 		fi;
+	done
 done
-if [ $money -eq $goal ]
-then
-	echo "You WON";
-else
-	echo "YOU LOST";
-fi
-
 
 
