a5d7ba58302d329d2f8dc426a13d734be0c232ab
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-04-20 19:54:47 +0530

    Refractor. added UseCase7(feature to determine wheather to continue simulaton to next month bassed on results) in gambling.sh

--- gambler.sh
+++ gambler.sh
@@ -25,39 +25,50 @@ daysLost=0;
 totalProfit=0;
 totalLoss=0;
 
-#Use case 6
-#Would also like to know my luckiest day where I won maximum and my unluckiest day where I lost maximum 
-index=0
-profitArray[((index++))]=0; #for 0th day result
-lossArray[((0))]=0;
-for ((i=0;i<days;i++))
-do
-	money=$stake;
-	lowlimit=`awk "BEGIN{print ($stake*(0.5))}"`
-	hilimit=`awk "BEGIN{print (($stake+$lowlimit))}"`
-	while (( `awk "BEGIN{print (($money<$hilimit))}"` )) && ((`awk "BEGIN{print (( $money > $lowlimit ))}"`))
-	do
-		gamble=$((RANDOM%2));
-		if [ $gamble -eq $isWin ]
-		then
-			money=$((money+1));
-		else
-			money=$((money-1));
-		fi;
-	done
-	if [ $money -eq 0]
-	then
-		lossArray[((index++))]=$lowlimit
-		stake=$((stake-lowlimit));
-		daysLost=$((daysLost+1));
-		totalLoss=$((totalLoss+lowlimit));
-	else
-		profitArray[((index++))]=$hilimit;
-		stake=$((stake+lowlimit));
-		daysWon=$((daysWon+1));
-		totalProfit=$((totalProfit+lowlimit));
-	fi
-done
-echo "LuckiestDay of month=$( findMax $profitArray )";
-echo "UnluckiestDay of month=$( findMax $lossArray )";
+#Use case 7
+#If won would like to continue playing next month or stop Gambling 
 
+
+function simulateGambling(){
+	index=0
+	profitArray[((index++))]=0; #for 0th day result
+	lossArray[((0))]=0;
+	for ((i=0;i<days;i++))
+	do
+		money=$stake;
+		lowlimit=`awk "BEGIN{print ($stake*(0.5))}"`
+		hilimit=`awk "BEGIN{print (($stake+$lowlimit))}"`
+		while (( `awk "BEGIN{print (($money<$hilimit))}"` )) && ((`awk "BEGIN{print (( $money > $lowlimit ))}"`))
+		do
+			gamble=$((RANDOM%2));
+			if [ $gamble -eq $isWin ]
+			then
+				money=$((money+1));
+			else
+				money=$((money-1));
+			fi;
+		done
+		if [ $money -eq 0]
+		then
+			lossArray[((index++))]=$lowlimit
+			stake=$((stake-lowlimit));
+			daysLost=$((daysLost+1));
+			totalLoss=$((totalLoss+lowlimit));
+		else
+			profitArray[((index++))]=$hilimit;
+			stake=$((stake+lowlimit));
+			daysWon=$((daysWon+1));
+			totalProfit=$((totalProfit+lowlimit));
+		fi
+	done
+	echo "LuckiestDay of month=$( findMax $profitArray )";
+	echo "UnluckiestDay of month=$( findMax $lossArray )";
+	#to determine wheather to play for next month or not
+	if [ $totalLoss -lt $totalProfit ]
+	then
+		$( simulateGambling );
+	fi
+
+}
+
+$( simulateGambling );
