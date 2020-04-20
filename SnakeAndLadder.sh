87d4694315adf5f36fb686fec586158be5088aad
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-04-20 23:07:22 +0530

    Refractor. added[UseCase2] feature for rolling a dice is added

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -2,9 +2,12 @@
 #This problem simulates a Snake and Ladder Game. The Player starts from 0 rolls the die to get a number between 1 to 6, finds a safe place, ladder or a snake keeps doing till the winning spot 100 is achieved.
 
 ##funtions
+#Use Case 1
+#Snake and Ladder game played with single player at start position 0
+
 function startGame()
 {
-	printf "\nWELCOME TO THE SNAKE AND LADDERS GAME"
+	printf "WELCOME TO THE SNAKE AND LADDERS GAME\n"
 	#Display board
 	displayBoard;
 }
@@ -13,8 +16,8 @@ function displayBoard(){
 	local rowlength=10;
 	local columndepth=10;
 	local position=100;
-	printf "Boards starting At 0"
-	printf "Board Layout \n=============  "
+	printf "\nBoards starting At 0"
+	printf "\nBoard Layout \n=============  "
 	for ((i=0;i<$rowlength;i++))
 	do
 		for ((j=0;j<$columndepth;j++))
@@ -28,8 +31,15 @@ function displayBoard(){
 			break;
 		fi
 	done
-	printf "Board ends at 1"
+	printf "\nBoard ends at 1"
 }
-#Use Case 1
-#Snake and Ladder game played with single player at start position 0
+
+#UseCase 2
+#The Player rolls the die to get a number between 1 to 6.
+function rollDice()
+{
+	echo "$((1+(RANDOM%6)))";
+}
+#maincode
 startGame;
+echo "$(rollDice)";
